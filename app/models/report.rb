6dcb8f4061dd535cebe75a1df5a060e040eee94a
koyama1058 <koyama1058tk@gmail.com>
2020-11-27 19:51:44 +0900

    アソシエーションの記述activehashに関するもの

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :EventDay
+    belongs_to_active_hash :MemberName
 end
