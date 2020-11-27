6d682a81b6b83cd5f769f018fa8ecaacd65fb8f8
koyama1058 <koyama1058tk@gmail.com>
2020-11-27 18:54:04 +0900

    マイグレーションファイルの作成

--- db/migrate/20201127084841_create_reports.rb
+++ db/migrate/20201127084841_create_reports.rb
@@ -1,7 +1,12 @@
 class CreateReports < ActiveRecord::Migration[6.0]
   def change
     create_table :reports do |t|
-
+      t.text :my_word,           null: false
+      t.string :member_word
+      t.integer :event_day_id,   null: false
+      t.integer :member_name_id, null: false
+      t.integer :sheets,         null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
