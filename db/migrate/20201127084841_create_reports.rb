3a70ef1ea03a86af86ccecf773b6851ed5acc477
koyama1058 <koyama1058tk@gmail.com>
2020-11-30 17:03:19 +0900

    reportsテーブルの修正

--- db/migrate/20201127084841_create_reports.rb
+++ db/migrate/20201127084841_create_reports.rb
@@ -1,10 +1,8 @@
 class CreateReports < ActiveRecord::Migration[6.0]
   def change
     create_table :reports do |t|
-      t.text :my_word,           null: false
-      t.string :member_word
+      t.integer :hope_id,        null: false
       t.integer :event_day_id,   null: false
-      t.integer :member_name_id, null: false
       t.integer :sheets,         null: false
       t.references :user, foreign_key: true
       t.timestamps
