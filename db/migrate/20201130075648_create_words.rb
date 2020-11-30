f0c28fcf7a7b92119475e6410365085582acf1b7
koyama1058 <koyama1058tk@gmail.com>
2020-11-30 17:03:52 +0900

    wordsテーブルの作成

---
+++ db/migrate/20201130075648_create_words.rb
@@ -0,0 +1,10 @@
+class CreateWords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :words do |t|
+      t.text :words,                 null: false
+      t.integer :order_id,           null: false
+      t.references :report,          foreign_key: true
+      t.timestamps
+    end
+  end
+end
