a68c10780bce05299dd08d017e7b4bcf43a22b97
koyama1058 <koyama1058tk@gmail.com>
2020-11-30 20:26:09 +0900

    reportsとwordsのアソシエーション記述

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :user
+  has_many :words
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :EventDay
-    belongs_to_active_hash :MemberName
+    belongs_to_active_hash :Hope
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,8 @@
 class Word < ApplicationRecord
+
+  belongs_to :report
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :Order
+  belongs_to_active_hash :MemberName
 end
