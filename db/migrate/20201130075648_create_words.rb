502bae146325b1b2d7e7b95feacbe758e599b13a
koyama1058 <koyama1058tk@gmail.com>
2020-11-30 20:17:42 +0900

    変更の反映

--- db/migrate/20201130075648_create_words.rb
+++ db/migrate/20201130075648_create_words.rb
@@ -1,6 +1,7 @@
 class CreateWords < ActiveRecord::Migration[6.0]
   def change
     create_table :words do |t|
+      t.integer :member_name_id, null: false
       t.text :words,                 null: false
       t.integer :order_id,           null: false
       t.references :report,          foreign_key: true
