ab4d47bfdf1f611d94c3cdf3d19e5564b6a6503e
koyama1058 <koyama1058tk@gmail.com>
2020-12-01 13:22:14 +0900

    投稿に関するcontrollerの記述

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,10 +5,23 @@ class ReportsController < ApplicationController
   end
 
   def new
-    @report = Report.new
+    report = Report.new
   end
 
   def create
+    @report = Report.new(report_params)
+    binding.pry
+    if @report.valid?
+      @report.save
+      redirect_to root_path
+    else 
+      render 'new'
+    end
+  end
 
+  private
+  
+  def report_params
+    params.permit(:hope_id, :event_day_id, :member_name_id, :sheets, :text).merge(user_id: current_user.id)
   end
 end
