f2b47ec0842a98f1f0a8790d63a46daf049132f8
koyama1058 <koyama1058tk@gmail.com>
2020-12-01 13:38:21 +0900

    バリデーションの記述

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,16 @@
 class Report < ApplicationRecord
   belongs_to :user
-  has_many :words
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :EventDay
     belongs_to_active_hash :Hope
+    belongs_to_active_hash :MemberName
+
+  with_options presence: true do
+    validates :hope_id, numericality: { less_than: 2 }
+    validates :event_day_id, inclusion: { in: 2..27 }
+    validates :member_name_id, inclusion: { in: 2..25 }
+    validates :sheets, numericality: {only_integer: true}
+    validates :text
+  end
 end
