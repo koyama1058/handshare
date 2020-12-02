806b7e748520fa7bed519614d56360d687c43539
koyama1058 <koyama1058tk@gmail.com>
2020-12-02 10:28:56 +0900

    userの投稿を取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @reports = Report.where(@user)
+    @reports = Report.where(user_id: @user)
   end
 end
