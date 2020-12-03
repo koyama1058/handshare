1e8a38ef4db2890d6310838752f6d803449abd0e
koyama1058 <koyama1058tk@gmail.com>
2020-12-03 09:45:47 +0900

    ユーザー詳細ページのエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @reports = Report.where(user_id: @user)
+    @reports = Report.where(user_id: @user).page(params[:page]).per(12)
   end
 end
